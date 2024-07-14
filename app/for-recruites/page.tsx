4300f970b95f576457f7926dddfa34baa205d55d
ayush kumar <7126924ak@gmail.com>
2024-07-14 15:24:13 +0530

    add for- recuriter page

---
+++ app/for-recruites/page.tsx
@@ -0,0 +1,26 @@
+import PlacemenrProcedure from '@/components/for-recruiters/PlacemenrProcedure'
+import Statistics from '@/components/why-recruit-us/Statistics'
+import Image from 'next/image'
+import React from 'react'
+
+export default function page() {
+    return (
+        <div>
+            <h1 className='text-5xl text-green-600 text-center font-bold my-10'>Past Recruiters</h1>
+            <section className='relative w-full h-[40vh] lg:h-[80vh]'>
+                <Image
+                    src='/for-recurites/company-list.png'
+                    alt='head'
+                    fill
+                    className='object-contain'
+                />
+            </section>
+
+            <section className='container my-32'>
+                <Statistics />
+
+                <PlacemenrProcedure />
+            </section>
+        </div>
+    )
+}
---
+++ components/for-recruiters/data.tsx
@@ -0,0 +1,44 @@
+import { Footprints } from "lucide-react";
+
+export const produreSteps = [
+    {
+        icon: <Footprints />,
+        step: 1,
+        description: "The Training and Placement Office, IIITT sends invitations along with the Job Notification Form (INF) to Organisers to participate in the campus placement drive."
+    },
+    {
+        icon: <Footprints />,
+        step: 2,
+        description: "The organization fills the JNF containing details of the job offer (pay package, Place of posting, allowances, and other bonuses). The JNF can be sent by mail to placement@iiitt.ac.in."
+    },
+    {
+        icon: <Footprints />,
+        step: 3,
+        description: "The organization details along with the job offer are made available to the students in a private group, requesting them to apply."
+    },
+    {
+        icon: <Footprints />,
+        step: 4,
+        description: "The Training and Placement Office, IIITT allots date(s) to an organization for their placement drive and confirms it with them."
+    },
+    {
+        icon: <Footprints />,
+        step: 5,
+        description: "The list of students who have applied is provided to the organization along with their Resumes."
+    },
+    {
+        icon: <Footprints />,
+        step: 6,
+        description: "Organizations come to the campus on the allotted date(s) and conduct tests and/or Interviews as per their recruitment process."
+    },
+    {
+        icon: <Footprints />,
+        step: 7,
+        description: "The organization is expected to furnish the list of selected candidates at the end of the assigned interview slot, preferably on the day of the campus visit."
+    },
+    {
+        icon: <Footprints />,
+        step: 8,
+        description: "The Training and Placement Office, IIITT also coordinates the signing of offer letters by the students selected to ensure that they reach the organization/company as soon as possible"
+    },
+]
