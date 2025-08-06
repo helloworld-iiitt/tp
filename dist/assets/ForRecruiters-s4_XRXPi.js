bc6eebbaadbbd8e5d4f27660dc32ded3be4af509
prateekgaurav <rvprateek@gmail.com>
2025-08-06 23:30:27 +0530

    TNP website update for 2025-26

--- dist/assets/ForRecruiters-s4_XRXPi.js
+++ dist/assets/ForRecruiters-s4_XRXPi.js
@@ -1,4 +1,4 @@
-import{c as s,j as e,m as n,k as c,l as r,n as o}from"./index-CMrCg5l5.js";import{S as l}from"./Statistics-DsfEIhwL.js";/**
+import{c as s,j as e,m as n,k as c,l as r,n as o}from"./index-DXU7jork.js";import{S as l}from"./Statistics-BQukhCGM.js";/**
  * @license lucide-react v0.436.0 - ISC
  *
  * This source code is licensed under the ISC license.
